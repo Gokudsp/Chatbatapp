af933949fbff658bd762f31787f3e687c526d7ce
GOKUL P <141717721+Gokudsp@users.noreply.github.com>
2023-12-08 19:09:58 -0800

    Update utils.js

--- src/Component/utils.js
+++ src/Component/utils.js
@@ -7,12 +7,25 @@ export const analyze = (text) => {
         return 'Im Chatbat create by Goku?'
         else if (text.includes('miss you'))
         return 'I miss you to?'
+        else if (text.includes('I love you'))
+        return 'I love you to dear?'
+        else if (text.includes('Who are you'))
+        return 'I am Goku?'
+        else if (text.includes('i miss you'))
+        return 'I miss you to?'
+        else if (text.includes('i want one qustions'))
+        return 'Tell me?'
+        else if (text.includes('Baby'))
+        return 'Tell me baby ?'
+        else if (text.includes('Dsp'))
+        return 'Dvi?'
+
 
     else if (text.includes('date'))
         return moment().format('MMMM Do YYYY')
     else if (text.includes("time"))
         return moment().format("h:mm:ss ")
-    else if (text.includes('google link'))
+    else if (text.includes('google'))
         return 'https://www.google.com'
 
     else if (text.includes('tnpsc'))
@@ -30,6 +43,56 @@ export const analyze = (text) => {
 
       else if (text.includes('salem'))
       return 'https://salem.nic.in/'
+
+      else if (text.includes('Rrb'))
+        return 'https://www.rrbbnc.gov.in/'
+
+        else if (text.includes('Tn velaivaaippu'))
+        return 'https://tnvelaivaaippu.gov.in/Empower/'
+
+        else if (text.includes('Indian railway'))
+        return 'https://indianrailways.gov.in/railwayboard/view_section.jsp?lang=0&id=0,5,373,2817'
+
+        else if (text.includes('Eshram'))
+        return 'https://eshram.gov.in/'
+
+        else if (text.includes('My govt'))
+        return 'https://www.mygov.in/work-at-mygov/'
+
+        else if (text.includes('Govt job'))
+        return 'https://www.govtjobsportal.in/'
+
+        else if (text.includes('Ncs'))
+        return 'https://www.ncs.gov.in/'
+
+        else if (text.includes('Hdhm'))
+        return 'https://hpr.ndhm.gov.in/en/users/login'
+
+        else if (text.includes('My aadhaar'))
+        return 'https://myaadhaar.uidai.gov.in/'
+
+
+        else if (text.includes('Uidai'))
+        return 'https://eaadhaar.uidai.gov.in/genricDownloadAadhaar'
+
+        else if (text.includes('Upsc'))
+        return'https://upsc.gov.in/'
+
+        else if( text.includes('Tn book'))
+        return'https://www.tntextbooks.co.in/?m=1'
+
+        else if( text.includes('Army'))
+        return'https://www.joinindianarmy.nic.in/Authentication.aspx'
+
+        else if( text.includes('Tn book old'))
+        return'https://www.tnpscjob.com/new-samacheer-kalvi-9th-books/'
+
+        else if( text.includes('Tnpds'))
+        return'https://tnpds.gov.in/'
+
+
+
+
 
     else if (text.includes("thank you"))
 
